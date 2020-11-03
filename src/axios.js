85a7cb0a9317d457eb65e3cf8d610d01d85daa25
fmarvelsk <fmarvelsk@gmail.com>
2020-11-03 12:13:48 +0100

    fixes

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://us-central1-ecommerce-62924.cloudfunctions.net/api'
+    baseURL: 'http://localhost:5001/ecommerce-62924/us-central1/api'
 });
 
 export default instance;
 
-/**http://localhost:5001/ecommerce-62924/us-central1/api **/
+/** **/
